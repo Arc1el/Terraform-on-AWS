0b123ff5ae47dcee541f03cacd2535294b60fb3d
Hyeonmin Kim <khm970514@gmail.com>
2023-04-13 10:02:49 +0900

    update comments

--- 05-Terraform-Loop-MetaArgs-Splat/05-01-Terraform-For-Loop-List-Map-Count/versions.tf
+++ 05-Terraform-Loop-MetaArgs-Splat/05-01-Terraform-For-Loop-List-Map-Count/versions.tf
@@ -1,10 +1,10 @@
 terraform {
-  # 20230413 기준 lts 1.4.4
+  # 20230413 기준 lts v1.4.4
   required_version = ">= 1.4.4"
   required_providers {
     aws = {
-      source  = "hashicorp/aws"2
-      # 20230413 기준 lts 4.0
+      source  = "hashicorp/aws"
+      # 20230413 기준 lts v4.62.0
       version = "~> 4.0"
     }
   }
