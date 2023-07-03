cac7e7854b9719919db55a695cd996bf16fe819f
Hyeonmin Kim <khm970514@gmail.com>
2023-07-03 17:20:38 +0900

    Update : vpc

---
+++ ecs-pipeline/01-vpc.tf
@@ -0,0 +1,32 @@
+locals {
+  region = "ap-northeast-2"
+  role_arn = "arn:aws:iam::759320821027:role/ducku-haha"
+  name   = "peteasy" # project name
+  key_name = "peteasy-key"
+  env_name = "prod"
+}
+
+module "vpc" {
+  source = "./modules/vpc"
+  vpc    = [{ 
+    name = "01.vpc-${local.name}-prod" 
+    cidr = "10.0.0.0/16" 
+
+  }]
+
+
+  subnet = [
+    {name="01.sub-${local.name}-prod-pub-a-01", cidr = "10.0.1.0/24",default_gateway = "igw" },
+    {name="02.sub-${local.name}-prod-pub-c-01", cidr = "10.0.2.0/24",default_gateway = "igw" },
+    {name="03.sub-${local.name}-prod-pri-a-01", cidr = "10.0.11.0/24",default_gateway = "nat" },
+    {name="04.sub-${local.name}-prod-pri-c-01", cidr = "10.0.22.0/24", default_gateway = "nat" },
+    {name="05.sub-${local.name}-prod-db-a-01", cidr = "10.0.31.0/24", default_gateway = "non" },
+    {name="06.sub-${local.name}-prod-db-c-01", cidr = "10.0.32.0/24", default_gateway = "non" },
+  ]
+  igw = true 
+  nat = ["01.sub-${local.name}-prod-pub-a-01", "02.sub-${local.name}-prod-pub-c-01"]
+
+  tags = { 
+    Env  = local.env_name
+  }
+}
