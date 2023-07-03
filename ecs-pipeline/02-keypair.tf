e393bc01374393234441814f7ccd19ec70c3791e
Hyeonmin Kim <khm970514@gmail.com>
2023-07-03 17:20:49 +0900

    Update : keypair

---
+++ ecs-pipeline/02-keypair.tf
@@ -0,0 +1,26 @@
+################################################################################
+# Key Pair 생성
+################################################################################
+
+# resource "aws_key_pair" "key1" {
+#   key_name   = "${local.name}-key"
+#   public_key = file("~/.ssh/ducku-key.pub")
+# }
+
+
+resource "tls_private_key" "tls_1" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "key_1" {
+  key_name   = local.key_name
+  public_key = tls_private_key.tls_1.public_key_openssh
+}
+
+resource "local_sensitive_file" "pem_file" {
+  filename = pathexpand("./${local.key_name}.pem")
+  file_permission = "600"
+  directory_permission = "700"
+  content = tls_private_key.tls_1.private_key_pem
+}
